5cbbf436f19ba838163f212c8c5cf66dc38b0452
송치윤 <songcchiyoon@gmail.com>
2017-08-28 18:07:02 +0900

    Convert to function

--- app_routes.js
+++ app_routes.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-var p1 = require('./routes/p1.js');
+var p1 = require('./routes/p1.js')(app); //require를 통해 갖고온 이것이 함수라는 의미
 app.use('/p1', p1);  //p1에게 오는 접속을 router에게 위임한다.
 
-var p2 = require('./routes/p2.js');
+var p2 = require('./routes/p2.js')(app); 
 
 app.use('/p2', p2);
 
--- routes/p1.js
+++ routes/p1.js
@@ -1,11 +1,16 @@
-var express = require('express');
-var route = express.Router();  //router를 호출
 
-route.get('/r1', function(req, res){
-  res.send('Hello /p1/r1');
-});
-route.get('/r2', function(req, res){
-  res.send('Hello /p1/r2');
-});
+module.exports = function(app){
+  var express = require('express');
+  var route = express.Router();  //router를 호출
 
-module.exports = route;
+  route.get('/r1', function(req, res){
+    res.send('Hello /p1/r1');
+  });
+  route.get('/r2', function(req, res){
+    res.send('Hello /p1/r2');
+  });
+  route.get('p3/r1', function(req, res){
+    res.send('Hello /p3/r1');
+  });
+  return route;
+};
