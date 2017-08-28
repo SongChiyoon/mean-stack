4712bfa9c4c3edb694aed624003ca560e4b48ca2
송치윤 <songcchiyoon@gmail.com>
2017-08-28 17:52:23 +0900

    Router seperates

---
+++ app_routes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var p1 = express.Router();  //router를 호출
+
+p1.get('/r1', function(req, res){
+  res.send('Hello /p1/r1');
+});
+p1.get('/r2', function(req, res){
+  res.send('Hello /p1/r2');
+});
+
+app.use('/p1', p1);  //p1에게 오는 접속을 router에게 위임한다.
+
+var p2 = express.Router();
+
+app.use('/p2', p2);
+
+p2.get('/r1', function(req, res){
+  res.send('Hello /p2/r1');
+});
+p2.get('/r2', function(req, res){
+  res.send('Hello /p2/r2');
+});
+app.listen(3003, function(){
+  console.log('connected');
+});
